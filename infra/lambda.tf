6d94c8d1fd9eff484f7c1116c9e58d596156c149
stefannaeve <stefan.naeve@hotmail.com>
2024-11-16 21:15:42 +0000

    Fixed bug in task 2

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -64,6 +64,7 @@ resource "aws_iam_role_policy_attachment" "lambda_bedrock_full_access" {
 
 resource "aws_sqs_queue" "sqs_queue" {
   name = "${var.prefix}_sqs_queue"
+  // Has to be higher than function timeout
   visibility_timeout_seconds = 65
 }
 
@@ -119,10 +120,33 @@ resource "aws_lambda_event_source_mapping" "lambda_sqs_trigger" {
   enabled          = true
 }
 
+# This was a weird one, i had a lot of problems with creating the aws cloudwatch
+# When i changed the input for the variable prefix
+# So i tried a lot to fix it, including trying to make lambda function dependant
+# On the creation of the log group, so it might destroy then build it again
+# So aws wouldnt make it. That did not work, so after a lot of searching i found
+# This method to be the one that actually worked. This might never have been
+# A problem if i just stuck to the prefix i originally made lmfao
+resource "null_resource" "set_log_retention" {
+  # Ensure this runs after the Lambda function is created
+  depends_on = [aws_lambda_function.sqs_lambda]
+
+  provisioner "local-exec" {
+    command = "aws logs put-retention-policy --log-group-name '/aws/lambda/${aws_lambda_function.sqs_lambda.function_name}' --retention-in-days 60"
+  }
+}
+
+/*
 resource "aws_cloudwatch_log_group" "sqs_log_group" {
   name              = "/aws/lambda/${var.prefix}_sqs_lambda_function"
-  retention_in_days = 7
+  retention_in_days = 60
+  
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy = false
+  }
 }
+*/
 
 output "lambda_function_name" {
   value = aws_lambda_function.sqs_lambda.function_name
