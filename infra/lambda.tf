c59a398059ff69b115e93d48c264fb70db5f7b69
stefannaeve <stefan.naeve@hotmail.com>
2024-11-25 21:22:49 +0000

    Testing restricting iam permissions

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -51,15 +51,31 @@ resource "aws_iam_role_policy" "lambda_s3_policy" {
   })
 }
 
+resource "aws_iam_policy" "lambda_bedrock_policy" {
+  name = "${var.prefix}_LambdaBedrockPolicy"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "bedrock:InvokeModel"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_bedrock_policy_attachment" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_bedrock_policy.arn
+}
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_bedrock_full_access" {
-  role = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonBedrockFullAccess"
 }
 
 resource "aws_sqs_queue" "sqs_queue" {
