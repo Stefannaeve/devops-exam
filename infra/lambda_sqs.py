661ae03aae73c834b2f53b9ba19c646bcd7d83e1
stefannaeve <stefan.naeve@hotmail.com>
2024-11-26 02:33:24 +0000

    Updated email notification variable to get handled from terraform code instead of github secrets

--- infra/lambda_sqs.py
+++ infra/lambda_sqs.py
@@ -5,7 +5,6 @@ import random
 import os
 
 # Frank; This is probalby be closer to what we need in production. Let's get funding first!
-
 bedrock_client = boto3.client("bedrock-runtime", region_name="us-east-1")
 s3_client = boto3.client("s3")
 
